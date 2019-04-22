215730b311616c7e43ba1012c614c620b3539990
SealTV <seal.matob@gmail.com>
2019-04-23 00:25:12 +0300

    Fix docker file and go code

--- Chapter02/kubia_go/main.go
+++ Chapter02/kubia_go/main.go
@@ -4,20 +4,34 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 func greet(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Recieved request from: %s", r.RemoteAddr)
+	log.Printf("Recieved request from: %s", r.Host)
 
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "Hello World! %s\n", time.Now())
-	fmt.Fprintf(w, "You've hit %s\n", r.Host)
+	fmt.Fprintf(w, "You've hit %s\n", r.RemoteAddr)
 }
 
 func main() {
 	http.HandleFunc("/", greet)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
+
+	go func() {
+		log.Println("Start listen")
+		if err := http.ListenAndServe(":8080", nil); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	cancel := make(chan os.Signal)
+	signal.Notify(cancel, syscall.SIGTERM)
+	signal.Notify(cancel, syscall.SIGINT)
+
+	sig := <-cancel
+	log.Printf("Stop service by signal: %v", sig)
 }
